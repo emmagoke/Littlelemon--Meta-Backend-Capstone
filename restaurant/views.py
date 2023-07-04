e8fa1d248c9dc9aedf38a1aa0e3663670bee739f
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-04 12:06:19 +0100

    Created the serializer and the API

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from .serializers import MenuItemSerializer, BookingSerializer
+from .models import Menu, Booking
 
 # Create your views here.
-def index(request):
-	return render(request, "index.html", {})
+
+class MenuItemView(generics.ListCreateAPIView):
+	queryset = Menu.objects.all()
+	serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+	queryset = Menu.objects.all()
+	serializer_class = MenuItemSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer

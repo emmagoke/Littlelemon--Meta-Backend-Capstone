4ce7e360455e8a1b20f342d10d07b1aaf7acfce5
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-05 12:24:13 +0100

    Added tests

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Menu
 
 # Create your tests here.
+class MenuTest(TestCase):
+
+	def test_get_menu_item(self):
+		item = Menu.objects.create(title='Icecream', price=10, inventory=100)
+		self.assertEqual(item, "Icecream : 10")
